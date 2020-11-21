403c01e8972e7d73c535fdf0f9747a045c6f825b
Sonnicon <sonniconn@gmail.com>
2020-11-21 15:15:28 +0000

    Initial Commit

---
+++ src/sonnicon/modtemplate/ModTemplate.java
@@ -0,0 +1,14 @@
+package sonnicon.modtemplate;
+
+import mindustry.mod.Mod;
+
+public class ModTemplate extends Mod{
+
+    @Override
+    public void init(){
+    }
+
+    @Override
+    public void loadContent(){
+    }
+}
