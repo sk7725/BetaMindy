c15ea256e0c76cb6af8f9b7c915383776eecf4d6
sk7725 <58885089+sk7725@users.noreply.github.com>
2021-07-04 21:38:01 +0900

    PUSH UNVERSIONED FILES TOO, INTELLIJ

---
+++ src/betamindy/world/blocks/logic/Disabler.java
@@ -0,0 +1,56 @@
+package betamindy.world.blocks.logic;
+
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import betamindy.world.blocks.production.payduction.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.logic.*;
+import mindustry.world.*;
+
+import static mindustry.Vars.tilesize;
+
+//todo make spinning claws drop stuff when over this block
+public class Disabler extends Block {
+    public Color zoneColor = Pal.health;
+    public Disabler(String name){
+        super(name);
+        update = true;
+        solid = false; //todo something special when a unit walks into it?
+    }
+
+    public class DisablerBuild extends Building {
+        public float heat = 0f;
+
+        @Override
+        public void updateTile(){
+            heat = Mathf.lerpDelta(heat, consValid() ? 1f : 0f, 0.05f);
+            for(Building p : proximity){
+                if(!(p.block instanceof Disabler)) p.control(LAccess.enabled, consValid() ? 0 : 1, 0, 0, 0);
+            }
+        }
+
+        @Override
+        public void drawSelect(){
+            super.drawSelect();
+            Lines.stroke(1f, Pal.remove);
+            for(Building p : proximity){
+                if(p.isValid() && !(p.block instanceof Disabler)){
+                    Lines.square(p.x, p.y, p.block.size * tilesize / 2f);
+                }
+            }
+            Draw.color();
+        }
+
+        @Override
+        public void draw(){
+            super.draw();
+            if(heat < 0.001f) return;
+            Draw.z(Layer.buildBeam);
+            Draw.color(zoneColor);
+            Fill.square(x, y, 5f * heat);
+            Draw.color();
+        }
+    }
+}
