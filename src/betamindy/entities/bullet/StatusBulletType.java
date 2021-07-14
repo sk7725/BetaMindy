ab727744bf66e4d1c7a15eb38dd5ef75bc199de4
sk7725 <58885089+sk7725@users.noreply.github.com>
2021-07-15 00:44:46 +0900

    UNVERSIONED FILES

---
+++ src/betamindy/entities/bullet/StatusBulletType.java
@@ -0,0 +1,78 @@
+package betamindy.entities.bullet;
+
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.util.*;
+import betamindy.content.*;
+import mindustry.entities.*;
+import mindustry.entities.bullet.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.type.*;
+
+public class StatusBulletType extends BulletType {
+    public StatusBulletType(StatusEffect s, float r){
+        status = s;
+        hitSize = drawSize = r * 2f;
+        lightRadius = r;
+        hitColor = lightColor = s.color;
+        lightOpacity = 0.2f;
+        maxRange = r;
+
+        speed = 0f;
+        lifetime = 500f;
+        collides = false;
+        collidesTiles = false;
+        collidesAir = false;
+        collidesGround = false;
+        keepVelocity = false;
+        hittable = false;
+        absorbable = false;
+
+        collidesTeam = false; //if collidesTeam is true, it will only affect allies. Otherwise, it affects all but allies.
+    }
+
+    @Override
+    public void init(Bullet b){
+        super.init(b);
+        MindyFx.zoneStart.at(b, maxRange);
+    }
+
+    @Override
+    public void update(Bullet b){
+        super.update(b);
+        if(collidesTeam) Units.nearby(b.team, b.x, b.y, maxRange, u -> u.apply(status, 8f));
+        else{
+            Units.nearbyEnemies(b.team, b.x, b.y, hitSize, hitSize, u -> {
+                if(u.dst2(b) <= maxRange * maxRange) u.apply(status, 8f);
+            });
+        }
+        if(Mathf.chanceDelta(0.3f)){
+            Tmp.v1.trns(Mathf.random(360f), maxRange).add(b);
+            status.effect.at(Tmp.v1);
+        }
+    }
+
+    @Override
+    public void draw(Bullet b){
+        Draw.color(status.color);
+        Lines.stroke(1f);
+        Lines.circle(b.x, b.y, Mathf.clamp((1f-b.fin())*20f)*maxRange);
+        Draw.color();
+
+        Draw.z(Layer.groundUnit - 0.01f);
+        Fill.light(b.x, b.y, Lines.circleVertices(maxRange), Mathf.clamp((1-b.fin())*20)*maxRange, Tmp.c4.set(status.color).a(0f), Tmp.c3.set(status.color).a(0.1f+0.25f*Mathf.sin(b.time()*0.02f)));
+        Draw.z(Layer.bullet);
+    }
+
+    @Override
+    public void despawned(Bullet b){}
+
+    @Override
+    public void hit(Bullet b, float x, float y){}
+
+    @Override
+    public float range(){
+        return maxRange;
+    }
+}
---
+++ src/betamindy/ui/MindyHints.java
@@ -0,0 +1,122 @@
+package betamindy.ui;
+
+import arc.*;
+import arc.func.*;
+import arc.input.*;
+import arc.struct.*;
+import arc.util.*;
+import betamindy.*;
+import betamindy.world.blocks.defense.*;
+import betamindy.world.blocks.environment.*;
+import betamindy.world.blocks.production.*;
+import betamindy.world.blocks.units.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.input.*;
+import mindustry.ui.fragments.HintsFragment.Hint;
+
+import static mindustry.Vars.*;
+
+public class MindyHints{
+    ObjectSet<String> events = new ObjectSet<>();
+
+    public void load(){
+        Core.app.post(() -> {
+            ui.hints.hints.and(MindyHint.values()).as();
+        });
+
+        Events.on(BlockBuildBeginEvent.class, event -> {
+            if(state != null && !state.rules.infiniteResources && event.breaking && event.unit == player.unit() && event.tile != null && (event.tile.block() instanceof Crystal)){
+                events.add("cryst");
+            }
+        });
+        /*Events.on(BlockBuildEndEvent.class, event -> {
+            if(!event.breaking && event.unit == player.unit() && event.tile != null && (event.tile.block() instanceof Mynamite)){
+                events.add("myna");
+            }
+        });*/
+        Events.on(ResetEvent.class, e -> {
+            events.clear();
+        });
+    }
+
+    public enum MindyHint implements Hint {
+        breakCrystal(() -> BetaMindy.hints.events.contains("cryst"), () -> false),
+        clearPipe(visibleDesktop, () -> (player.unit() instanceof BlockUnitc) && (((BlockUnitc)player.unit()).tile().block instanceof ClearPipe), () -> false),
+        spike(() -> control.input.block instanceof Spike, () -> false);
+        //mynamite(() -> BetaMindy.hints.events.contains("myna"), () -> false);
+        //note: the bundles keays for these are...camelcased?!
+
+        @Nullable
+        String text;
+        int visibility = visibleAll;
+        Hint[] dependencies = {};
+        boolean finished, cached;
+        Boolp complete, shown = () -> true;
+
+        MindyHint(Boolp complete){
+            this.complete = complete;
+        }
+
+        MindyHint(int visiblity, Boolp complete){
+            this(complete);
+            this.visibility = visiblity;
+        }
+
+        MindyHint(Boolp shown, Boolp complete){
+            this(complete);
+            this.shown = shown;
+        }
+
+        MindyHint(int visiblity, Boolp shown, Boolp complete){
+            this(complete);
+            this.shown = shown;
+            this.visibility = visiblity;
+        }
+
+        @Override
+        public boolean finished(){
+            if(!cached){
+                cached = true;
+                finished = Core.settings.getBool(name() + "-hint-done", false);
+            }
+            return finished;
+        }
+
+        @Override
+        public void finish(){
+            Core.settings.put(name() + "-hint-done", finished = true);
+        }
+
+        @Override
+        public String text(){
+            if(text == null){
+                text = Vars.mobile && Core.bundle.has("hint." + name() + ".mobile") ? Core.bundle.get("hint." + name() + ".mobile") : Core.bundle.get("hint." + name());
+                if(!Vars.mobile) text = text.replace("tap", "click").replace("Tap", "Click");
+            }
+            return text;
+        }
+
+        @Override
+        public boolean complete(){
+            return complete.get();
+        }
+
+        @Override
+        public boolean show(){
+            return shown.get() && (dependencies.length == 0 || !Structs.contains(dependencies, d -> !d.finished()));
+        }
+
+        @Override
+        public int order(){
+            return ordinal();
+        }
+
+        @Override
+        public boolean valid(){
+            return (Vars.mobile && (visibility & visibleMobile) != 0) || (!Vars.mobile && (visibility & visibleDesktop) != 0);
+        }
+    }
+}
