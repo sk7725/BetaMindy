ce3528eb4097ca53d6a5343a60a757c2ca511ca6
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2021-07-24 17:27:21 +0200

    Update Shop.java

--- src/betamindy/world/blocks/storage/Shop.java
+++ src/betamindy/world/blocks/storage/Shop.java
@@ -34,6 +34,8 @@ public class Shop extends Block{
 
         update = solid = hasItems = outputsItems = true;
 
+        itemCapacity = 0;
+
         configurable = true;
     }
 
@@ -116,6 +118,11 @@ public class Shop extends Block{
         @Override
         public boolean acceptItem(Building source, Item item){
             return false;
+        }
+
+        @Override
+        public void updateTile(){
+            super.updateTile();
         }
 
         @Override
