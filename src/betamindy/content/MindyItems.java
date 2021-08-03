5f5a34d6a183902a6a1adcf3fd88ffd4da783fb4
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2021-08-03 12:05:58 +0200

    Update MindyItems.java

--- src/betamindy/content/MindyItems.java
+++ src/betamindy/content/MindyItems.java
@@ -59,7 +59,7 @@ public class MindyItems implements ContentList {
             cost = 2;
         }};
 
-        tensor = new AnimatedItem("tensor",Color.valueOf("7ef2cf")){{
+        tensor = new AnimatedItem("tensor",Color.valueOf("00bfa2")){{
             hardness = 8;
             cost = 3;
 
