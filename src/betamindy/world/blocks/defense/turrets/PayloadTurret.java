f3eae764e845e68768bd8fc58a47c667a2c4f626
MEEP of Faith <meepmatthew@gmail.com>
2021-10-29 21:03:09 -0700

    copy a ton of code over from PM

--- src/betamindy/world/blocks/defense/turrets/PayloadTurret.java
+++ src/betamindy/world/blocks/defense/turrets/PayloadTurret.java
@@ -1,32 +1,29 @@
 package betamindy.world.blocks.defense.turrets;
 
 import arc.*;
-import arc.graphics.*;
 import arc.graphics.g2d.*;
 import arc.math.*;
-import arc.struct.ObjectSet;
+import arc.math.geom.*;
+import arc.struct.*;
 import arc.util.*;
 import arc.util.io.*;
-import betamindy.BetaMindy;
+import betamindy.*;
 import betamindy.content.*;
-import mindustry.Vars;
-import mindustry.content.*;
-import mindustry.entities.*;
+import mindustry.*;
 import mindustry.entities.bullet.*;
 import mindustry.gen.*;
 import mindustry.graphics.*;
 import mindustry.logic.*;
-import mindustry.ui.Cicon;
 import mindustry.world.*;
 import mindustry.world.blocks.defense.turrets.*;
 import mindustry.world.blocks.payloads.*;
 import mindustry.world.meta.*;
 
-import static arc.Core.atlas;
+import static arc.Core.*;
 import static mindustry.Vars.*;
 
 @SuppressWarnings("unchecked")
-public class PayloadTurret extends Turret {
+public class PayloadTurret extends Turret{
     /** Base damage multiplier */
     public float damage = 1.3f;
     /** Range multiplier for block payloads. Unused. */
@@ -39,14 +36,12 @@ public class PayloadTurret extends Turret {
     public BulletType homingShootType = MindyBullets.homingPay;
     /** Payload draw offset, draw scale */
     public float payloadOffset = 15f,  payloadScale = 0.8f;
-    /** Payload fire offset*/
-    public float payloadShootOffset = 15f;
     /** Maximum accepted payload size */
     public float maxPaySize = 4.5f;
+    public float payloadSpeed = 0.7f, payloadRotateSpeed = 5f;
+    public float loadTime = 20f;
 
-    public Effect acceptEffect = MindyFx.cannonAccept;
-    public TextureRegion topRegion;
-    public boolean useTopRegion = false; //set automatically
+    public TextureRegion inRegion, topRegion;
 
     protected ObjectSet<Block> homingBlocks = new ObjectSet<Block>(2);
 
@@ -68,8 +63,8 @@ public class PayloadTurret extends Turret {
     @Override
     public void load(){
         super.load();
-        topRegion = atlas.find(name + "-top");
-        useTopRegion = atlas.isFound(topRegion);
+        inRegion = atlas.find(name + "-in", "factory-in-" + size);
+        topRegion = atlas.find(name + "-top", "factory-top-" + size);
     }
 
     @Override
@@ -87,26 +82,128 @@ public class PayloadTurret extends Turret {
         stats.add(Stat.damage, "@", Core.bundle.format("stat.dphealth", damage * maxDamagePercent * tilesize / range));//dmg/health*range
     }
 
+    //stolen from PayloadBlock
+    public static boolean blends(Building build, int direction){
+        int size = build.block.size;
+        int trns = build.block.size/2 + 1;
+        Building accept = build.nearby(Geometry.d4(direction).x * trns, Geometry.d4(direction).y * trns);
+        return accept != null &&
+            accept.block.outputsPayload &&
+
+            //if size is the same, block must either be facing this one, or not be rotating
+            ((accept.block.size == size
+                && Math.abs(accept.tileX() - build.tileX()) % size == 0 //check alignment
+                && Math.abs(accept.tileY() - build.tileY()) % size == 0
+                && ((accept.block.rotate && accept.tileX() + Geometry.d4(accept.rotation).x * size == build.tileX() && accept.tileY() + Geometry.d4(accept.rotation).y * size == build.tileY())
+                || !accept.block.rotate
+                || !accept.block.outputFacing)) ||
+
+                //if the other block is smaller, check alignment
+                (accept.block.size != size &&
+                    (accept.rotation % 2 == 0 ? //check orientation; make sure it's aligned properly with this block.
+                        Math.abs(accept.y - build.y) <= Math.abs(size * tilesize - accept.block.size * tilesize)/2f : //check Y alignment
+                        Math.abs(accept.x - build.x) <= Math.abs(size * tilesize - accept.block.size * tilesize)/2f   //check X alignment
+                    )) && (!accept.block.rotate || accept.front() == build || !accept.block.outputFacing) //make sure it's facing this block
+            );
+    }
+
     public class PayloadTurretBuild<T extends Payload> extends TurretBuild{
         public @Nullable T payload;
-        protected float payheat;
+        public Vec2 payVector = new Vec2();
+        public float payRotation, loadProgress;
+        public boolean rotating, loading, loaded, shooting;
 
         @Override
         public void updateTile(){
             unit.ammo(Mathf.num(payload != null) * unit.type().ammoCapacity);
-            payheat = Mathf.lerpDelta(payheat, 1f, 0.1f);
+            if(!validateTarget()) target = null;
 
-            super.updateTile();
+            wasShooting = false;
+
+            recoil = Mathf.lerpDelta(recoil, 0f, restitution);
+            heat = Mathf.lerpDelta(heat, 0f, cooldown);
+
+            if(unit != null){
+                unit.health(health);
+                unit.rotation(rotation);
+                unit.team(team);
+                unit.set(x, y);
+            }
+
+            if(logicControlTime > 0){
+                logicControlTime -= Time.delta;
+            }
+
+            if(hasAmmo()){
+                updateLoading();
+
+                if(timer(timerTarget, targetInterval)){
+                    findTarget();
+                }
+
+                if(validateTarget()){
+                    boolean canShoot = true;
+
+                    if(isControlled()){ //player behavior
+                        targetPos.set(unit.aimX(), unit.aimY());
+                        canShoot = unit.isShooting();
+                    }else if(logicControlled()){ //logic behavior
+                        canShoot = logicShooting;
+                    }else{ //default AI behavior
+                        targetPosition(target);
+
+                        if(Float.isNaN(rotation)){
+                            rotation = 0;
+                        }
+                    }
+
+                    float targetRot = angleTo(targetPos);
+
+                    if(shouldTurn()){
+                        turnToTarget(targetRot);
+                    }
+
+                    if(Angles.angleDist(rotation, targetRot) < shootCone && canShoot){
+                        wasShooting = true;
+                        updateShooting();
+                    }
+                }
+
+                if(shooting){
+                    updateLaunching();
+                }
+            }else{
+                moveInPayload(false); //Rotating is done elsewhere
+            }
+
+            if(acceptCoolant){
+                updateCooling();
+            }
         }
 
-        public TextureRegion payloadIcon(){
-            if(payload instanceof BuildPayload){
-                return ((BuildPayload)payload).build.block.icon(Cicon.full);
+        public void updateLoading(){
+            if(!loaded){
+                loadPayload();
+                rotatePayload();
+
+                if(!loading && !rotating){
+                    loaded = true;
+                }
             }
-            else if(payload instanceof UnitPayload){
-                return ((UnitPayload)payload).unit.type().icon(Cicon.full);
+        }
+
+        public void loadPayload(){
+            loadProgress = Mathf.approach(loadProgress, shootLength - recoil, payloadSpeed * delta());
+            loading = !Mathf.equal(loadProgress, shootLength - recoil, 0.01f);
+        }
+
+        public void rotatePayload(){
+            if(!Angles.within(payRotation, rotation + rotationOffset(), 0.01f)){
+                payRotation = Angles.moveToward(payRotation, rotation + rotationOffset(), payloadRotateSpeed * edelta());
+                rotating = true;
+            }else{
+                rotating = false;
             }
-            return Core.atlas.find("error");
         }
 
         public float rotationOffset(){
@@ -115,29 +212,85 @@ public class PayloadTurret extends Turret {
 
         @Override
         public void draw(){
-            super.draw();
-            if(payload != null) {
-                TextureRegion payIcon = payloadIcon();
-                tr2.trns(rotation, -recoil + payloadOffset);
-                Draw.mixcol(team.color, 1f - payheat);
-                Draw.rect(payIcon, x + tr2.x, y + tr2.y, payIcon.width * Draw.scl * Draw.xscl * payloadScale * payheat, payIcon.height * Draw.scl * Draw.yscl * payloadScale * payheat, rotation + rotationOffset());
-                Draw.reset();
-                if(useTopRegion){
-                    tr2.trns(rotation, -recoil);
-                    Draw.rect(topRegion, x + tr2.x, y + tr2.y, rotation - 90);
+            Draw.rect(baseRegion, x, y);
+
+            //draw input
+            for(int i = 0; i < 4; i++){
+                if(blends(i)){
+                    Draw.rect(inRegion, x, y, (i * 90f) - 180f);
                 }
             }
+
+            drawPayload();
+            if(atlas.isFound(topRegion)){
+                Draw.z(Layer.blockOver + 0.1f);
+                Draw.rect(topRegion, x, y);
+            }
+
+            Draw.z(Layer.turret);
+
+            tr2.trns(rotation, -recoil);
+
+            Drawf.shadow(region, x + tr2.x - elevation, y + tr2.y - elevation, rotation - 90);
+            drawer.get(this);
+
+            if(heatRegion != Core.atlas.find("error")){
+                heatDrawer.get(this);
+            }
+        }
+
+        public void drawPayload(){
+            if(payload != null){
+                updatePayload();
+
+                if(loaded){
+                    loadProgress = shootLength - recoil;
+                    payRotation = rotation + rotationOffset();
+                }
+
+                Draw.z(loaded ? Layer.turret + 0.01f : Layer.blockOver);
+                //payload.draw() but with rotation
+                Drawf.shadow(payload.x(), payload.y(), payload.size() * 2f);
+                Draw.rect(payload.icon(), payload.x(), payload.y(), payRotation);
+            }
+        }
+
+        public boolean blends(int direction){
+            return PayloadBlock.blends(this, direction);
+        }
+
+        @Override
+        public boolean shouldTurn(){
+            return super.shouldTurn() && loaded;
         }
 
         @Override
         protected void updateShooting(){
-            if(payload != null) super.updateShooting();
+            reload += delta() * peekAmmo().reloadMultiplier * baseReloadSpeed();
+
+            if(reload > reloadTime && loaded && !shooting){
+                shooting = true;
+            }
+        }
+
+        protected void updateLaunching(){
+            loadProgress = 0f;
+            loaded = false;
+            shoot(peekAmmo());
+            shooting = false;
+            reload %= reloadTime;
+        }
+
+        protected void shoot(BulletType type){
+            super.shoot(type);
+            recoil = recoilAmount;
+            loadProgress = 0f;
         }
 
         @Override
         protected void bullet(BulletType type, float angle){
             if((payload instanceof BuildPayload) && homingBlocks.contains(((BuildPayload)payload).block())) type = homingShootType;
-            tr.trns(rotation, payloadShootOffset, Mathf.range(xRand));
+            tr.trns(rotation, shootLength, Mathf.range(xRand));
             float lifeScl = type.scaleVelocity ? Mathf.clamp(Mathf.dst(x + tr.x, y + tr.y, targetPos.x, targetPos.y) / type.range(), minRange / type.range(), range / type.range()) : 1f;
 
             type.create(this, team, x + tr.x, y + tr.y, angle, -1f, 1f, lifeScl, payload);
@@ -147,11 +300,11 @@ public class PayloadTurret extends Turret {
 
         @Override
         public double sense(LAccess sensor){
-            switch(sensor){
-                case ammo: return Mathf.num(payload != null);
-                case ammoCapacity: return 1;
-                default: return super.sense(sensor);
-            }
+            return switch(sensor){
+                case ammo -> Mathf.num(payload != null);
+                case ammoCapacity -> 1;
+                default -> super.sense(sensor);
+            };
         }
 
         @Override
@@ -162,8 +315,7 @@ public class PayloadTurret extends Turret {
 
         @Override
         public boolean hasAmmo(){
-            //you can always rotate, but never shoot if there's no payload
-            return true;
+            return payload != null && hasArrived();
         }
 
         @Override
@@ -184,17 +336,32 @@ public class PayloadTurret extends Turret {
 
         @Override
         public boolean acceptPayload(Building source, Payload pay){
-            return payload == null && pay.size() / tilesize <= maxPaySize;
+            return payload == null && pay.fits(maxPaySize);
         }
 
         @Override
         public void handlePayload(Building source, Payload pay){
             payload = (T)pay;
-            Tmp.v1.set(source).sub(this).clamp(-size * tilesize / 2f, -size * tilesize / 2f, size * tilesize / 2f, size * tilesize / 2f);
-            acceptEffect.at(Tmp.v1.x + x, Tmp.v1.y + y);
-            payheat = 0f;
+            this.payVector.set(source).sub(this).clamp(-size * tilesize / 2f, -size * tilesize / 2f, size * tilesize / 2f, size * tilesize / 2f);
+            this.payRotation = pay.rotation() + rotationOffset();
 
-            updatePayload(source.angleTo(this));
+            updatePayload();
+        }
+
+        /** */
+        public void moveInPayload(boolean rotate){
+            if(payload == null) return;
+
+            updatePayload();
+
+            if(rotate){
+                payRotation = Angles.moveToward(payRotation, block.rotate ? rotdeg() : 90f, payloadRotateSpeed * edelta());
+            }
+            payVector.approach(Vec2.ZERO, payloadSpeed * delta());
+        }
+
+        public boolean hasArrived(){
+            return payVector.isZero(0.01f);
         }
 
         @Override
@@ -215,9 +382,14 @@ public class PayloadTurret extends Turret {
             if(payload != null) payload.dump();
         }
 
-        public void updatePayload(float r){
+        public void updatePayload(){
             if(payload != null){
-                payload.set(x, y, r);
+                if(hasArrived()){
+                    tr.trns(rotation, loadProgress);
+                    payload.set(x + tr.x, y + tr.y, payRotation);
+                }else{
+                    payload.set(x + payVector.x, y + payVector.y, payRotation);
+                }
             }
         }
 
@@ -225,19 +397,41 @@ public class PayloadTurret extends Turret {
         public void write(Writes write){
             super.write(write);
 
-            if(mobile) BetaMindy.mobileUtil.writePayload(payload, write);
-            else Payload.write(payload, write);
+            write.f(payVector.x);
+            write.f(payVector.y);
+            write.f(payRotation);
+            Payload.write(payload, write);
+
+            write.f(loadProgress);
+            write.bool(loaded);
+            write.bool(shooting);
+            write.bool(loading);
+            write.bool(rotating);
         }
 
         @Override
         public void read(Reads read, byte revision){
             super.read(read, revision);
 
-            if(mobile) payload = BetaMindy.mobileUtil.readPayload(read);
-            else payload = Payload.read(read);
+            if(revision >= 2){
+                payVector.set(read.f(), read.f());
+                payRotation = read.f();
+                payload = Payload.read(read);
+
+                loadProgress = read.f();
+                loaded = read.bool();
+                shooting = read.bool();
+                loading = read.bool();
+                rotating = read.bool();
+            }else{
+                if(mobile) payload = BetaMindy.mobileUtil.readPayload(read);
+                else payload = Payload.read(read);
+            }
         }
 
-        public void drawPayload(){
+        @Override
+        public byte version(){
+            return 2;
         }
 
         @Override
